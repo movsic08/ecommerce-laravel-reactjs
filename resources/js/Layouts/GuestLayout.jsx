d9e17e39e02156318ea770bb2cdacd2d9c8fe45f
movisc08 <lejer08@gmail.com>
2024-06-14 09:35:24 +0800

    login ui

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,18 +1,26 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link } from "@inertiajs/react";
 
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
-            </div>
-
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {children}
-            </div>
+        <div className="min-h-screen flex flex-col">
+            <header className="w-full flex items-center justify-center p-4  text-mainText bg-header">
+                <img src="LOGO.svg" alt="logo" className=" mr-1" />
+                <h1 className="text-4xl font-medium">MadeByHands</h1>
+            </header>
+            <main className="flex-grow flex flex-col justify-center items-center bg-gray-100">
+                <div className="w-full flex flex-col items-center justify-center sm:max-w-md mt-6 px-8 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                    {children}
+                </div>
+            </main>
+            <footer className="w-full flex flex-col text-slate-50 items-center justify-center p-4 bg-footer">
+                <h1 className="text uppercase  tracking-widest ">
+                    MadeByHands
+                </h1>
+                <span className=" text-xs  font-light text-slate-50">
+                    Copyright © Louis All Right Reserved.
+                </span>
+            </footer>
         </div>
     );
 }
--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,38 +1,44 @@
-import { useEffect } from 'react';
-import Checkbox from '@/Components/Checkbox';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { useEffect } from "react";
+import Checkbox from "@/Components/Checkbox";
+import GuestLayout from "@/Layouts/GuestLayout";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import { Head, Link, useForm } from "@inertiajs/react";
 
 export default function Login({ status, canResetPassword }) {
     const { data, setData, post, processing, errors, reset } = useForm({
-        email: '',
-        password: '',
+        email: "",
+        password: "",
         remember: false,
     });
 
     useEffect(() => {
         return () => {
-            reset('password');
+            reset("password");
         };
     }, []);
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('login'));
+        post(route("login"));
     };
 
     return (
         <GuestLayout>
             <Head title="Log in" />
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
-
-            <form onSubmit={submit}>
+            {status && (
+                <div className="mb-4 font-medium text-sm text-green-600">
+                    {status}
+                </div>
+            )}
+            <h1 className=" font-bold text-3xl py-4 uppercase text-mainText">
+                Login
+            </h1>
+            <form onSubmit={submit} className=" w-full">
                 <div>
                     <InputLabel htmlFor="email" value="Email" />
 
@@ -44,7 +50,7 @@ export default function Login({ status, canResetPassword }) {
                         className="mt-1 block w-full"
                         autoComplete="username"
                         isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
+                        onChange={(e) => setData("email", e.target.value)}
                     />
 
                     <InputError message={errors.email} className="mt-2" />
@@ -60,38 +66,49 @@ export default function Login({ status, canResetPassword }) {
                         value={data.password}
                         className="mt-1 block w-full"
                         autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
+                        onChange={(e) => setData("password", e.target.value)}
                     />
 
                     <InputError message={errors.password} className="mt-2" />
                 </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
-                        />
-                        <span className="ms-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
+                <div className=" w-full flex mt-4 items-center justify-between">
+                    <div className="block ">
+                        <label className="flex items-center">
+                            <Checkbox
+                                name="remember"
+                                checked={data.remember}
+                                onChange={(e) =>
+                                    setData("remember", e.target.checked)
+                                }
+                            />
+                            <span className="ms-2 text-sm text-gray-600">
+                                Remember me
+                            </span>
+                        </label>
+                    </div>
                     {canResetPassword && (
                         <Link
-                            href={route('password.request')}
+                            href={route("password.request")}
                             className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         >
                             Forgot your password?
                         </Link>
                     )}
+                </div>
 
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
+                <div className="flex items-center w-full justify-end mt-4">
+                    <PrimaryButton
+                        className="w-full text-center"
+                        disabled={processing}
+                    >
+                        Sign in
                     </PrimaryButton>
                 </div>
             </form>
+            <div className=" text-mainText text-sm  border-t-2 border-gray-200 flex items-center justify-between w-full mt-6 pt-6 ">
+                <h1>Become a seller</h1>
+                <h1>Create an account</h1>
+            </div>
         </GuestLayout>
     );
 }
