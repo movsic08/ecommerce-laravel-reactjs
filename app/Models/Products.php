0ea63e2accdc7439f5ce47d91541c4b781706e4d
movsic08 <lejero08@gmail.com>
2024-06-19 16:41:37 +0800

    Seeder: Products and Categories

---
+++ app/Models/Products.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+  use HasFactory;
+
+  protected $fillable = ['product_name', 'rating', 'quantity', 'seller_id', 'category_id', 'type'];
+
+  public function seller()
+  {
+    return $this->belongsTo(User::class);
+  }
+
+  public function category()
+  {
+    return $this->belongsTo(Categories::class);
+  }
+  public function images()
+  {
+    return $this->hasMany(ProductsImages::class);
+  }
+
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,33 @@
 
 namespace Database\Seeders;
 
+use Database\Factories\ProductFactory;
+use App\Models\Products;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    User::factory(50)->create();
+    $this->call([
+      CategoriesSeeder::class,
+    ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    $this->call([
+        // Other seeders...
+      ProductsSeeder::class,
+    ]);
+
+    // User::factory()->create([
+    //     'name' => 'Test User',
+    //     'email' => 'test@example.com',
+    // ]);
+  }
 }
