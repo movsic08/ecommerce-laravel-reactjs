a7c3e044eecb247e9d623e3341e722fea768203f
movsic08 <lejero08@gmail.com>
2024-06-27 16:48:45 +0800

    FrontEnd: Added the artist of the month in home

--- resources/js/Layouts/UserAuthenticatedLayout.jsx
+++ resources/js/Layouts/UserAuthenticatedLayout.jsx
@@ -46,7 +46,7 @@ export default function UserAuthenticatedLayout({ user, header, children }) {
 
     return (
         <div className="min-h-screen bg-white">
-            <nav className="  shadow drop-shadow-md border-b border-gray-100">
+            <nav className="  shadow drop-shadow-md border-b bg-white border-gray-100">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
                         <div className="shrink-0 flex items-center">
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,27 +1,111 @@
+import StarRating from "@/Components/StarRating";
 import AuthenticatedLayout from "@/Layouts/UserAuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
+import frame_1 from "../assets/img/Frame_1.png";
 
 export default function Dashboard({ auth }) {
+    const images = [
+        {
+            url: "https://i.ytimg.com/vi/wy7-Y-C3P34/maxresdefault.jpg",
+            alt: "Random Image 1",
+            caption: "Caption for Random Image 1",
+        },
+        {
+            url: "https://media-cdn.socastsrm.com/wordpress/wp-content/blogs.dir/1050/files/2022/02/diy-capiz-lamp.png",
+            alt: "Random Image 2",
+            caption: "Caption for Random Image 2",
+        },
+        // Add more images as needed
+    ];
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            // header={
-            //     <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-            //         Dashboard
-            //     </h2>
-            // }
-        >
-            <Head title="Dashboard" />
+        <AuthenticatedLayout user={auth.user}>
+            <Head title="Home" />
+            <div className="-mt-[5rem] h-screen w-full  -z-20 flex items-center justify-center ">
+                <img
+                    className=" w-full h-full object-cover"
+                    src={frame_1}
+                    alt=""
+                />
+            </div>
+            <div className=" bg-[#FEF3F0] flex flex-col items-center justify-center h-screen w-screen p-6s">
+                <h1 className=" font-bold  text-4xl py-8">
+                    Artist of the Month
+                </h1>
+                <div className=" flex items-start justify-center ">
+                    <img
+                        src="https://2.img-dpreview.com/files/p/E~C1000x0S4000x4000T1200x1200~articles/3925134721/0266554465.jpeg"
+                        className=" rounded-full object-cover h-52 w-52"
+                        alt=""
+                    />
+                    <div className=" ml-8">
+                        <h2 className=" font-semibold text-3xl">Seller Name</h2>
+                        <div className="mt-4">Store name</div>
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
-                        </div>
+                        <table className="min-w-full overflow-hidden">
+                            <tr className="text-left">
+                                <td className=" font-bold pr-6 pt-2">Name</td>
+                                <td className="pt-2 font-italic">John Doe</td>
+                            </tr>
+                            <tr>
+                                <td className=" font-bold pr-6 pt-2">
+                                    Location
+                                </td>
+                                <td className="pt-2 font-italic">
+                                    Lucap Wharf
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className=" font-bold pr-6 pt-2">Join</td>
+                                <td className="pt-2 font-italic">
+                                    March 24, 2002
+                                </td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
+
+                <div className="grid mt-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center">
+                    {" "}
+                    <Link
+                        href=""
+                        className="bg-gray-100 drop-shadow-lg rounded relative overflow-hidden"
+                    >
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEEWvj-brrQLo63rYQe-vQ8sUi5495fskgQw&s"
+                            alt=""
+                            className="mx-auto h-48 w-full object-cover"
+                        />
+                        <div className="p-4 text-center h-fit flex flex-col">
+                            <p className="line-clamp-2 h-[5vw] grow ">name</p>
+                            <div className="flex  flex-none flex-col ">
+                                <StarRating rating={4} />
+                                <p className="font-semibold">Php 200</p>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link
+                        href=""
+                        className="bg-gray-100 drop-shadow-lg rounded relative overflow-hidden"
+                    >
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEEWvj-brrQLo63rYQe-vQ8sUi5495fskgQw&s"
+                            alt=""
+                            className="mx-auto h-48 w-full object-cover"
+                        />
+                        <div className="p-4 text-center h-fit flex flex-col">
+                            <p className="line-clamp-2 h-[5vw] grow ">name</p>
+                            <div className="flex  flex-none flex-col ">
+                                <StarRating rating={4} />
+                                <p className="font-semibold">Php 200</p>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
             </div>
+
+            {/* <div className=" h-screen w-screen bg-white text-gray-900">
+                You're logged in!
+            </div> */}
         </AuthenticatedLayout>
     );
 }
