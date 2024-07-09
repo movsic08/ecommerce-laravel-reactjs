97a06f242c7b0282cd1f5f42958bdce625a2ef8c
movsic08 <lejero08@gmail.com>
2024-07-09 13:46:23 +0800

    Fixed: Change class to classname of spinnerloading component

--- resources/js/Components/SpinnerLoading.jsx
+++ resources/js/Components/SpinnerLoading.jsx
@@ -1,7 +1,7 @@
 export default function SpinnerLoading({}) {
     return (
         <>
-            <div class="border-gray-100 h-5 w-5 animate-spin rounded-full border m-1 border-t-orange-800" />
+            <div className="border-gray-100 h-5 w-5 animate-spin rounded-full border m-1 border-t-orange-800" />
         </>
     );
 }
