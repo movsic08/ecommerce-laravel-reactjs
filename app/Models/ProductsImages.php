39d47bd98fd397fef0eb4fb8e31eb2f2938781a9
movsic08 <lejero08@gmail.com>
2024-07-23 01:25:25 +0800

    Model: added the correct table name

--- app/Models/ProductsImages.php
+++ app/Models/ProductsImages.php
@@ -10,6 +10,7 @@ class ProductsImages extends Model
   use HasFactory;
   protected $fillable = ['product_id', 'image_path'];
 
+  protected $table = 'product_images';
   public function product()
   {
     return $this->belongsTo(Products::class);
