79380f968651602e248c0a07ec37b374965ed370
movsic08 <lejero08@gmail.com>
2024-07-23 12:41:29 +0800

    Auth: added log for debugging

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -44,7 +45,7 @@ class AuthenticatedSessionController extends Controller
   public function adminLogin(LoginRequest $request): RedirectResponse
   {
     $request->authenticate();
-
+    Log::info('User authenticated:', ['user' => auth()->user()]);
     $request->session()->regenerate();
 
     return redirect()->intended(route('admin.index', absolute: false));
