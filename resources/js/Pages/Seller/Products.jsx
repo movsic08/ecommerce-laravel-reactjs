00fce4dd7be88323d1dfc83336519518f7bc4391
movsic08 <lejero08@gmail.com>
2024-07-23 11:28:27 +0800

    Removed: Console log

--- resources/js/Pages/Seller/Products.jsx
+++ resources/js/Pages/Seller/Products.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 
 export default function Products() {
     const { products } = usePage().props;
-    console.log(products);
+    // console.log(products);
     return (
         <>
             <SellerAuthenticatedLayout>
