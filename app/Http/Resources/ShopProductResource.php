8773768f037ba9eba7f94cfc267b7261007387f1
movsic08 <lejero08@gmail.com>
2024-07-31 11:46:44 +0800

    Resource: Added product list resource maker

---
+++ app/Http/Resources/ShopProductResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ShopProductResource extends JsonResource
+{
+  /**
+   * Transform the resource into an array.
+   *
+   * @return array<string, mixed>
+   */
+
+  public static $wrap = false;
+  public function toArray(Request $request): array
+  {
+    return [
+      'id' => $this->id,
+      'product_name' => $this->product_name,
+      'rating' => $this->rating,
+      'sold' => $this->sold,
+      'quantity' => $this->quantity,
+      'price' => $this->price,
+      'description' => $this->description,
+      'is_verified' => $this->is_verified,
+      'category_id' => $this->category_id,
+      'type' => $this->type,
+      'created_at' => $this->created_at,
+      'updated_at' => $this->updated_at,
+    ];
+  }
+}
