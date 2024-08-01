222e4bf2a734813c463c8079e393b45ee2e3bc77
movsic08 <lejero08@gmail.com>
2024-08-01 14:51:49 +0800

    Mail: Update the message for verified seller.

--- resources/views/Mail/account/VerifiedSeller.blade.php
+++ resources/views/Mail/account/VerifiedSeller.blade.php
@@ -9,7 +9,8 @@
   <h1>Your status has been updated</h1>
 
   <p>Hello Seller,</p>
-  <p>Your status is now {{ $sellerData->is_verified == 1 ? 'Verified' : 'Not Verified' }}.</p>
+  <p>Your current status is: <strong>{{ $sellerData->is_verified == 1 ? 'Verified' : 'Not Verified' }}</strong>.</p>
+  <p>Please log in to your account to complete the data update process. Once your information is up to date, you will be able to start uploading your products.</p>
 </body>
 
 </html>
