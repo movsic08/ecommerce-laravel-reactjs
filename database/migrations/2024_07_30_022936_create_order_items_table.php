63ba4f8f99349d52b86c8ff4876ffa6dc2dd8b28
movsic08 <lejero08@gmail.com>
2024-08-03 21:22:20 +0800

    Fixed references

--- database/migrations/2024_07_30_022936_create_order_items_table.php
+++ database/migrations/2024_07_30_022936_create_order_items_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
       $table->integer('quantity');
       $table->decimal('price', 8, 2);
       $table->unsignedBigInteger('seller_id');
-      $table->foreign('seller_id')->reference('id')->on('sellers')->onDelete('cascade');
+      $table->foreign('seller_id')->references('id')->on('sellers')->onDelete('cascade');
       $table->string('shop_name');
       $table->string('product_name');
       $table->string('status');
