2848a53227f00d0e3f6c4ffaf1153bd74bfd7445
movsic08 <lejero08@gmail.com>
2024-08-03 21:41:00 +0800

    added sellers address to resouce

--- app/Http/Resources/AdminResourceOfSellers.php
+++ app/Http/Resources/AdminResourceOfSellers.php
@@ -19,6 +19,7 @@ class AdminResourceOfSellers extends JsonResource
       'id' => $this->id,
       'name' => $this->first_name . ' ' . $this->last_name,
       'email' => $this->email,
+      'address' => $this->address,
       'seller_id' => $this->seller_id,
       'created_at' => Carbon::parse($this->created_at)->format('M d, Y'),
     ];
