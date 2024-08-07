b7d5d5b367ef80fc2b81c82dca1897649eef6d38
movsic08 <lejero08@gmail.com>
2024-08-07 15:52:37 +0800

    Added seller pic, status and id

--- app/Http/Resources/AdminResourceOfSellers.php
+++ app/Http/Resources/AdminResourceOfSellers.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class AdminResourceOfSellers extends JsonResource
 {
@@ -20,7 +21,9 @@ class AdminResourceOfSellers extends JsonResource
       'name' => $this->first_name . ' ' . $this->last_name,
       'email' => $this->email,
       'address' => $this->address,
-      'seller_id' => $this->seller_id,
+      'seller_id' => $this->seller->seller_id,
+      'status' => $this->seller->is_verified,
+      'seller_picture' => $this->seller->shop_picture_path ? Storage::url($this->seller->shop_picture_path) : null,
       'created_at' => Carbon::parse($this->created_at)->format('M d, Y'),
     ];
   }
