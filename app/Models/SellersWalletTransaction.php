ee53897150877e9e20277d1f48a601bde8e700f6
movsic08 <lejero08@gmail.com>
2024-08-23 23:27:44 +0800

    Migration & Model: Added Sellers Wallet Transaction

---
+++ app/Models/SellersWalletTransaction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SellersWalletTransaction extends Model
+{
+  use HasFactory;
+
+  protected $table = 'sellers_wallet_transactions';
+
+  protected $fillable = [
+    'wallet_id',
+    'type',
+    'amount',
+    'reference_number'
+  ];
+}
