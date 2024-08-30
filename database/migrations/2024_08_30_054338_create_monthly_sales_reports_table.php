58b83e0df878523d7762da6f08878fd77fd3d90a
movsic08 <lejero08@gmail.com>
2024-08-30 13:51:46 +0800

    Migration: aadded weekly and montly table

---
+++ database/migrations/2024_08_30_054338_create_monthly_sales_reports_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('monthly_sales_reports', function (Blueprint $table) {
+      $table->id();
+      $table->unsignedBigInteger('seller_id');
+      $table->decimal('total_sales', 10, 2);
+      $table->integer('total_orders');
+      $table->date('report_date');
+      $table->timestamps();
+
+      $table->foreign('seller_id')->references('id')->on('sellers')->onDelete('cascade');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('monthly_sales_reports');
+  }
+};
