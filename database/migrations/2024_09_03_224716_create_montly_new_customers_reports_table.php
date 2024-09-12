3c3e891e73b77c4140be858adb75e9da03d8d1a2
movsic08 <lejero08@gmail.com>
2024-09-12 17:13:49 +0800

    Migration: added fake data in monthly montly_new_customers_reports

--- database/migrations/2024_09_03_224716_create_montly_new_customers_reports_table.php
+++ database/migrations/2024_09_03_224716_create_montly_new_customers_reports_table.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -17,6 +19,33 @@ return new class extends Migration
       $table->integer('customers_count');
       $table->timestamps();
     });
+
+    $this->monthlyNewCustomersReportFakeData();
+  }
+
+  private function monthlyNewCustomersReportFakeData(): void
+  {
+    $months = [
+      '2024-01',
+      '2024-02',
+      '2024-03',
+      '2024-04',
+      '2024-05',
+      '2024-06',
+      '2024-07',
+      '2024-08'
+    ];
+
+    foreach ($months as $month) {
+      $endOfTheMonth = Carbon::createFromFormat('Y-m', $month)->endOfMonth()->setTime(23, 59, 58);
+
+      DB::table('montly_new_customers_reports')->insert([
+        'month' => $month,
+        'customers_count' => rand(2, 500),
+        'created_at' => $endOfTheMonth,
+        'updated_at' => $endOfTheMonth,
+      ]);
+    }
   }
 
   /**
