4aaecd663aeefaa48093a60f899405ea102be0fe
movsic08 <lejero08@gmail.com>
2024-09-12 17:10:22 +0800

    Migration: added fake data in monthly monthly_sales_reports

--- database/migrations/2024_08_30_054338_create_monthly_sales_reports_table.php
+++ database/migrations/2024_08_30_054338_create_monthly_sales_reports_table.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -21,8 +23,39 @@ return new class extends Migration
 
       $table->foreign('seller_id')->references('id')->on('sellers')->onDelete('cascade');
     });
+
+    $this->monthlySalesReportFakeData();
   }
 
+  private function monthlySalesReportFakeData(): void
+  {
+    $sellersId = [1, 2, 3, 4];
+    $months = [
+      '2024-01-30',
+      '2024-02-30',
+      '2024-03-30',
+      '2024-04-30',
+      '2024-05-30',
+      '2024-06-30',
+      '2024-07-30',
+      '2024-08-30'
+    ];
+
+    foreach ($months as $month) {
+      foreach ($sellersId as $id) {
+        $endOfTheMonth = Carbon::createFromFormat('Y-m-d', $month)->endOfMonth()->setTime(23, 59, 58);
+
+        DB::table('monthly_sales_reports')->insert([
+          'seller_id' => $id,
+          'total_sales' => rand(5000, 10000),
+          'total_orders' => rand(50, 200),
+          'report_date' => $endOfTheMonth,
+          'created_at' => $endOfTheMonth,
+          'updated_at' => $endOfTheMonth,
+        ]);
+      }
+    }
+  }
   /**
    * Reverse the migrations.
    */
