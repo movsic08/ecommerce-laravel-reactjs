6a3ff8911563a913433c487e8901c739feefa0ea
movsic08 <lejero08@gmail.com>
2024-09-12 17:02:31 +0800

    Migration: added fake data in monthly sales report

--- database/migrations/2024_09_03_225923_create_montly_payments_reports_table.php
+++ database/migrations/2024_09_03_225923_create_montly_payments_reports_table.php
@@ -1,7 +1,9 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -18,6 +20,34 @@ return new class extends Migration
       $table->integer('payments_count');
       $table->timestamps();
     });
+
+    $this->createMontlyPaymentReportData();
+  }
+
+  public function createMontlyPaymentReportData(): void
+  {
+    $months = [
+      '2024-01',
+      '2024-02',
+      '2024-03',
+      '2024-04',
+      '2024-05',
+      '2024-06',
+      '2024-07',
+      '2024-08'
+    ];
+
+    foreach ($months as $month) {
+      $endOfTheMonth = Carbon::createFromFormat('Y-m', $month)->endOfMonth()->setTime(23, 59, 58);
+
+      DB::table('montly_payments_reports')->insert([
+        'month' => $month,
+        'total_payments' => rand(5000, 10000),
+        'payments_count' => rand(50, 200),
+        'created_at' => $endOfTheMonth,
+        'updated_at' => $endOfTheMonth,
+      ]);
+    }
   }
 
   /**
