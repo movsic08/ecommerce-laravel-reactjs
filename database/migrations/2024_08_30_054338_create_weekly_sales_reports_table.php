a6e5c4668ddc83a260aa74b3988e4c9a841449a8
movsic08 <lejero08@gmail.com>
2024-09-12 16:45:39 +0800

    Migration: added fake data in weeklysalesreport

--- database/migrations/2024_08_30_054338_create_weekly_sales_reports_table.php
+++ database/migrations/2024_08_30_054338_create_weekly_sales_reports_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\WeeklySalesReport;
+use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -21,6 +23,36 @@ return new class extends Migration
 
       $table->foreign('seller_id')->references('id')->on('sellers')->onDelete('cascade');
     });
+
+    $this->weeklySalesReportDump();
+  }
+
+  private function weeklySalesReportDump(): void
+  {
+
+    $startDate = Carbon::create(2024, 1, 1)->startOfWeek(Carbon::SUNDAY);
+    $endDate = Carbon::create(2024, 9, 8);
+
+    $sellersId = [1, 2, 3, 4];
+
+    $fakeData = [];
+
+    while ($startDate->lessThanOrEqualTo($endDate)) {
+      foreach ($sellersId as $seller) {
+        $fakeData[] = [
+          'seller_id' => $seller,
+          'total_sales' => rand(1000, 5000),
+          'total_orders' => rand(10, 50),
+          'report_date' => $startDate->format('Y-m-d'),
+          'created_at' => $startDate->copy()->startOfDay(),
+          'updated_at' => $startDate->copy()->startOfDay(),
+        ];
+      }
+
+      $startDate->addWeek();
+    }
+
+    WeeklySalesReport::insert($fakeData);
   }
 
   /**
