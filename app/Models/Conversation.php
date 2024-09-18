c2dc4000190e25150dc1565ec3e2ef834752b474
Elmer <lejero08@gmail.com>
2024-09-18 18:31:14 +0800

    Migration: Added Conversation

---
+++ app/Models/Conversation.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Conversation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id1',
+        'user_id2'
+    ];
+}
