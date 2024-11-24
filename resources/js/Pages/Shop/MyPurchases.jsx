2a8bb2880f24a9aa3e75a708947b793a5df2925c
Elmer <lejero08@gmail.com>
2024-11-25 07:43:50 +0800

    Responsiveness in my purchases

--- resources/js/Pages/Shop/MyPurchases.jsx
+++ resources/js/Pages/Shop/MyPurchases.jsx
@@ -62,143 +62,144 @@ export default function MyPurchases({ auth }) {
             >
                 <Head title="My Purchases" />
                 <ToastContainer />
-                <div className="py-6 max-w-7xl sm:px-6 lg:px-8">
-                    <h1 className="mb-6 text-3xl font-bold">My Purchases</h1>
+                <div className="lg:mx-auto max-w-7xl 2xl:min-w-[80rem] ">
+                    <div className="py-6 mx-auto sm:px-6 lg:px-8">
+                        <h1 className="mb-6 text-3xl font-bold">My Purchases</h1>
 
-                    <div className="mb-6">
-                        <ul className="flex justify-around overflow-y-auto border-b">
-                            {tabs.map((tab) => (
-                                <li
-                                    key={tab.id}
-                                    className={`cursor-pointer p-4 flex-col md:flex-row text-slate-700 flex items-center space-x-2 transition-colors duration-200 ${activeTab === tab.id
+                        <div className="mb-6">
+                            <ul className="flex justify-around overflow-y-auto border-b">
+                                {tabs.map((tab) => (
+                                    <li
+                                        key={tab.id}
+                                        className={`cursor-pointer p-4 flex-col md:flex-row text-slate-700 flex items-center space-x-2 transition-colors duration-200 ${activeTab === tab.id
                                             ? "border-b-2 border-themeColor text-themeColor"
                                             : "hover:text-themeColor"
-                                        }`}
-                                    onClick={(e) => handleChangeTab(tab.id)}
-                                >
-                                    {tab.icon}
-                                    <span className="whitespace-nowrap">
-                                        {tab.label}
-                                    </span>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
+                                            }`}
+                                        onClick={(e) => handleChangeTab(tab.id)}
+                                    >
+                                        {tab.icon}
+                                        <span className="whitespace-nowrap">
+                                            {tab.label}
+                                        </span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
 
-                    <div className="space-y-4">
-                        <Suspense fallback={<div>Loading...</div>}>
-                            {activeTab === "toPay" && (
-                                <ToPay
-                                    toPay={purchases.data
-                                        .map((bulk) => ({
-                                            ...bulk,
-                                            items: bulk.items.filter(
-                                                (product) =>
-                                                    product.status ==
-                                                    "order placed" &&
-                                                    !product.is_preparing &&
-                                                    !product.is_ready_for_pickup &&
-                                                    !product.is_picked_up &&
-                                                    !product.is_in_transit &&
-                                                    !product.is_out_for_delivery &&
-                                                    !product.is_delivered &&
-                                                    !product.is_cancelled
-                                            ),
-                                        }))
-                                        .filter(
-                                            (bulk) => bulk.items.length > 0
-                                        )}
-                                />
-                            )}
-                            {activeTab === "toShip" && (
-                                <ToShip
-                                    toShipData={purchases.data
-                                        .map((bulk) => ({
-                                            ...bulk,
-                                            items: bulk.items.filter(
-                                                (product) =>
-                                                    product.status ==
-                                                    "pending" ||
-                                                    product.status ==
-                                                    "preparing"
-                                            ),
-                                        }))
-                                        .filter(
-                                            (bulk) => bulk.items.length > 0
-                                        )}
-                                />
-                            )}
-                            {activeTab === "toReceive" && (
-                                <ToReceive
-                                    toReceiveData={purchases.data
-                                        .map((bulk) => ({
-                                            ...bulk,
-                                            items: bulk.items.filter(
-                                                (product) =>
-                                                    product.status == "shipped"
-                                            ),
-                                        }))
-                                        .filter(
-                                            (bulk) => bulk.items.length > 0
-                                        )}
-                                />
-                            )}
-                            {activeTab === "completed" && (
-                                <Completed
-                                    completedData={purchases.data
-                                        .map((bulk) => ({
-                                            ...bulk,
-                                            items: bulk.items.filter(
-                                                (product) =>
-                                                    product.status ==
-                                                    "delivered" &&
-                                                    product.is_delivered
-                                            ),
-                                        }))
-                                        .filter(
-                                            (bulk) => bulk.items.length > 0
-                                        )}
-                                />
-                            )}
-                            {activeTab === "toRate" && (
-                                <ToRate
-                                    myReviewsData={myReviews.data}
-                                    toRateData={purchases.data
-                                        .map((bulk) => ({
-                                            ...bulk,
-                                            items: bulk.items.filter(
-                                                (product) =>
-                                                    product.status ==
-                                                    "delivered" &&
-                                                    product.is_delivered &&
-                                                    !product.is_rated
-                                            ),
-                                        }))
-                                        .filter(
-                                            (bulk) => bulk.items.length > 0
-                                        )}
-                                />
-                            )}
-                            {activeTab === "cancelled" && (
-                                <Cancelled
-                                    cancelled={purchases.data
-                                        .map((bulk) => ({
-                                            ...bulk,
-                                            items: bulk.items.filter(
-                                                (product) =>
-                                                    product.status ==
-                                                    "cancelled" &&
-                                                    product.is_cancelled
-                                            ),
-                                        }))
-                                        .filter(
-                                            (bulk) => bulk.items.length > 0
-                                        )}
-                                />
-                            )}
-                        </Suspense>
-                    </div>
-                </div>
+                        <div className="space-y-4">
+                            <Suspense fallback={<div>Loading...</div>}>
+                                {activeTab === "toPay" && (
+                                    <ToPay
+                                        toPay={purchases.data
+                                            .map((bulk) => ({
+                                                ...bulk,
+                                                items: bulk.items.filter(
+                                                    (product) =>
+                                                        product.status ==
+                                                        "order placed" &&
+                                                        !product.is_preparing &&
+                                                        !product.is_ready_for_pickup &&
+                                                        !product.is_picked_up &&
+                                                        !product.is_in_transit &&
+                                                        !product.is_out_for_delivery &&
+                                                        !product.is_delivered &&
+                                                        !product.is_cancelled
+                                                ),
+                                            }))
+                                            .filter(
+                                                (bulk) => bulk.items.length > 0
+                                            )}
+                                    />
+                                )}
+                                {activeTab === "toShip" && (
+                                    <ToShip
+                                        toShipData={purchases.data
+                                            .map((bulk) => ({
+                                                ...bulk,
+                                                items: bulk.items.filter(
+                                                    (product) =>
+                                                        product.status ==
+                                                        "pending" ||
+                                                        product.status ==
+                                                        "preparing"
+                                                ),
+                                            }))
+                                            .filter(
+                                                (bulk) => bulk.items.length > 0
+                                            )}
+                                    />
+                                )}
+                                {activeTab === "toReceive" && (
+                                    <ToReceive
+                                        toReceiveData={purchases.data
+                                            .map((bulk) => ({
+                                                ...bulk,
+                                                items: bulk.items.filter(
+                                                    (product) =>
+                                                        product.status == "shipped"
+                                                ),
+                                            }))
+                                            .filter(
+                                                (bulk) => bulk.items.length > 0
+                                            )}
+                                    />
+                                )}
+                                {activeTab === "completed" && (
+                                    <Completed
+                                        completedData={purchases.data
+                                            .map((bulk) => ({
+                                                ...bulk,
+                                                items: bulk.items.filter(
+                                                    (product) =>
+                                                        product.status ==
+                                                        "delivered" &&
+                                                        product.is_delivered
+                                                ),
+                                            }))
+                                            .filter(
+                                                (bulk) => bulk.items.length > 0
+                                            )}
+                                    />
+                                )}
+                                {activeTab === "toRate" && (
+                                    <ToRate
+                                        myReviewsData={myReviews.data}
+                                        toRateData={purchases.data
+                                            .map((bulk) => ({
+                                                ...bulk,
+                                                items: bulk.items.filter(
+                                                    (product) =>
+                                                        product.status ==
+                                                        "delivered" &&
+                                                        product.is_delivered &&
+                                                        !product.is_rated
+                                                ),
+                                            }))
+                                            .filter(
+                                                (bulk) => bulk.items.length > 0
+                                            )}
+                                    />
+                                )}
+                                {activeTab === "cancelled" && (
+                                    <Cancelled
+                                        cancelled={purchases.data
+                                            .map((bulk) => ({
+                                                ...bulk,
+                                                items: bulk.items.filter(
+                                                    (product) =>
+                                                        product.status ==
+                                                        "cancelled" &&
+                                                        product.is_cancelled
+                                                ),
+                                            }))
+                                            .filter(
+                                                (bulk) => bulk.items.length > 0
+                                            )}
+                                    />
+                                )}
+                            </Suspense>
+                        </div>
+                    </div></div>
             </UserAuthenticatedLayout>
         </>
     );
